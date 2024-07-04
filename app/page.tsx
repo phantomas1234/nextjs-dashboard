62e25d197964c459b68acb9d9281fcdf973e307c
Niko Sonnenschein <niko.sonnenschein@gmail.com>
2024-07-04 14:39:38 +0100

    Fix hero png

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,9 @@ export default function Page() {
           {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
         </div>
